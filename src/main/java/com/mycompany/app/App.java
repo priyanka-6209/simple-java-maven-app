e5a5ea47e26846b0a1e1b95e14ac5f724e0cff7b
priyanka-6209 <priyankapriyadarshini422@gmail.com>
2019-01-30 10:59:36 +0000

    automate

--- src/main/java/com/mycompany/app/App.java
+++ src/main/java/com/mycompany/app/App.java
@@ -6,7 +6,7 @@ package com.mycompany.app;
 public class App
 {
 
-    private final String message = "Hello World  automated trigger!!!"
+    private final String message = "Hello World  automated trigger!!!";
     public App() {}
 
     public static void main(String[] args) {
